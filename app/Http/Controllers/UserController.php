400338850e0d99075f47724c131648e3401fe18f
Marc Enzo <emarc237@gmail.com>
2020-04-17 00:42:04 +0100

    perf(auth): send sms to user at signup
    Send password to user via SMS at registration.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
@@ -21,7 +22,7 @@ class UserController extends Controller
 		$users = User::all();
 		return response()->json(['users' => $users]);
 	}
-	
+
 	/**
 	 * Get user account.
 	 *
@@ -37,7 +38,7 @@ class UserController extends Controller
 			return response()->json(['message' => "Impossible de retrouver l'utilisateur.", 'exception' => $e->getMessage()], 500);
 		}
 	}
-	
+
 	/**
 	 * Store a newly created user.
 	 *
@@ -52,19 +53,25 @@ class UserController extends Controller
 			'name' => 'required',
 			'phone' => 'required',
 		]);
-		
+
 		try {
 			$data = $request->input();
 			$exists = User::all()->where('phone', $data['phone'])->first() != null;
-			
+
 			if (!$exists) {
-				$data['password'] = Hash::make(Str::random(8));
-				
+				$password = Str::random(8);
+				$data['password'] = Hash::make($password);
+
 				$user = new User($data);
 				$user->saveOrFail();
 				
-				// Send password to user via SMS
+				$client = new Client();
 				
+				// Send password to user via SMS
+				$message = "Bienvenue sur DayCash, votre compte a été créé avec succès. Votre mot de passe est: " . $password;
+				$phone = Str::startsWith($data['phone'], ['237', '+237']) ? $data['phone'] : '237' . $data['phone'];
+				$client->get("http://obitsms.com/api/bulksms?username=" . env('SMS_USER') . "&password=" . env('SMS_PASSWORD') . "&sender=" . env('SMS_SENDER') . "&destination=" . $phone . "&message=" . $message);
+
 				return response()->json(['message' => "Votre compte a été ouvert avec succès. Vous recevrez le mot de passe par message sous peu.", 'user' => $user], 201);
 			} else {
 				return response()->json((['message' => "Vous possédez déjà un compte, veuillez vous connecter."]), 500);
@@ -73,7 +80,7 @@ class UserController extends Controller
 			return response()->json(['message' => "Une erreur est survenue lors de la création du compte.", 'exception' => $e->getMessage()], 500);
 		}
 	}
-	
+
 	/**
 	 * Update user account.
 	 *
@@ -86,19 +93,19 @@ class UserController extends Controller
 		try {
 			$user = User::findOrFail($user);
 			$data = $request->input();
-			
+
 			if (isset($data['password']))
+				// Send password to user via SMS?
 				$data['password'] = Hash::make($data['password']);
-				// Send password to user via SMS
-			
+
 			$user->update($data);
-			
+
 			return response()->json(['message' => "Votre compte a été mis à jour avec succès.", 'user' => $user]);
 		} catch (\Exception $e) {
 			return response()->json(['message' => "Une erreur est survenue lors de la mise à jour de votre compte.", 'exception' => $e->getMessage()], 500);
 		}
 	}
-	
+
 	/**
 	 * Delete user account.
 	 *
--- app/User.php
+++ app/User.php
@@ -10,32 +10,42 @@ use Laravel\Lumen\Auth\Authorizable;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, Authorizable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'phone', 'password', 'points', 'active'
-    ];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
+	use Authenticatable, Authorizable;
+	
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'phone', 'password', 'points', 'active', 'email', 'city',
+	];
+	
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password',
+	];
 	
 	/**
 	 * The casted attributes.
 	 *
 	 * @var array
 	 */
-    protected $casts = [
-    	'active' => 'boolean'
-    ];
+	protected $casts = [
+		'active' => 'boolean'
+	];
+	
+	/**
+	 * Get the bets of the user.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+	public function bets()
+	{
+		return $this->hasMany(Bet::class, 'user');
+	}
 }
